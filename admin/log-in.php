e2af31f3ffdbaac3ceaca7e4445d036b806cf772
Jomer Dorego <jdorego06@gmail.com>
2022-04-07 13:46:03 +0800

    Display categories to user UI

--- admin/log-in.php
+++ admin/log-in.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/admin.css">
+  <link rel="stylesheet" href="../css/admins.css">
   <title>Log in Admin</title>
 </head>
 <body>
