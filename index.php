fd75afe5e9ed1a283517a9839c89865a44b8a7e4
Jomer Dorego <jdorego06@gmail.com>
2022-04-08 10:12:55 +0800

    done front to back

--- index.php
+++ index.php
@@ -17,37 +17,9 @@
         <div class="container">
             <h2 class="text-center">Explore Foods</h2>
 
-            <?php
-                // create sql query to display the categories from data base
-                $query = "SELECT * FROM tbl_category WHERE active = 'Yes' AND featured = 'Yes' LIMIT 3";
+            <!-- FUNCTION THAT WILL DISPLAY ALL CATEGORIES RETURN FROM THE DATABASE -->
+            <?php displayCategories($conn); ?>
 
-                //execute the qeury to so that we can get all the data
-                $res = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-                //Fecth all the data that result of the query return;
-                $categories = mysqli_fetch_all($res, MYSQLI_ASSOC);
-
-                //free the result form the query
-                mysqli_free_result($res);
-
-                // print_r($categories.);
-                foreach($categories as $category): ?>
-                    <a href="category-foods.html">
-                        <div class="box-3 float-container">
-                            <?php
-                                if($category['image_name'] == 'Image is not currently available'){
-                                    echo '<div class="error">IMAGE IN NOT CURRENTLY AVAILABLE</div>';
-                                } else {
-                                    ?>
-                                        <img src="<?php echo ROOT_URL;?>images/category/<?php echo $category['image_name'];?>" alt="<?php echo $category['image_name'];?>" class="img-responsive img-curve">
-                                    <?php
-                                }
-                            ?>                            
-                            <h3 class="float-text text-white"><?php echo $category['title']; ?></h3>
-                        </div>
-                    </a>
-                <?php endforeach;
-            ?>
             <div class="clearfix"></div>
         </div>
     </section>
@@ -58,118 +30,84 @@
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
 
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="order.html" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Smoky Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-burger.jpg" alt="Chicke Hawain Burger" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Nice Burger</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Food Title</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
-            <div class="food-menu-box">
-                <div class="food-menu-img">
-                    <img src="images/menu-momo.jpg" alt="Chicke Hawain Momo" class="img-responsive img-curve">
-                </div>
-
-                <div class="food-menu-desc">
-                    <h4>Chicken Steam Momo</h4>
-                    <p class="food-price">$2.3</p>
-                    <p class="food-detail">
-                        Made with Italian Sauce, Chicken, and organice vegetables.
-                    </p>
-                    <br>
-
-                    <a href="#" class="btn btn-primary">Order Now</a>
-                </div>
-            </div>
-
+            <!-- FUNCTION THAT WILL DISPLAY ALL CATEGORIES RETURN FROM THE DATABASE -->
+            <?php displayFeaturedFoods($conn); ?>
 
             <div class="clearfix"></div>
-
-            
-
         </div>
 
         <p class="text-center">
-            <a href="#">See All Foods</a>
+            <a href="<?php echo ROOT_URL; ?>foods.php">See All Foods</a>
         </p>
     </section>
     <!-- fOOD Menu Section Ends Here -->
 <?php include('partials-front/footer.php'); ?>
+
+
+<?php 
+    function displayCategories($conn) {
+        // create sql query to display the categories from data base
+        $query = "SELECT * FROM tbl_category WHERE active = 'Yes' AND featured = 'Yes' LIMIT 3";
+
+        //execute the qeury to so that we can get all the data
+        $res = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+        //Fecth all the data that result of the query return;
+        $categories = mysqli_fetch_all($res, MYSQLI_ASSOC);
+
+        //free the result form the query
+        mysqli_free_result($res);
+
+        // print_r($categories.);
+        foreach($categories as $category): ?>
+            <a href="<?php echo ROOT_URL; ?>category-foods.php?id=<?php echo $category['id']; ?>&&title=<?php echo $category['title']; ?>">
+                <div class="box-3 float-container">
+                    <?php
+                        if($category['image_name'] == 'Image is not currently available'){
+                            echo '<div class="error">IMAGE IN NOT CURRENTLY AVAILABLE</div>';
+                        } else {
+                            ?>
+                                <img src="<?php echo ROOT_URL;?>images/category/<?php echo $category['image_name'];?>" alt="<?php echo $category['image_name'];?>" class="img-responsive img-curve">
+                            <?php
+                        }
+                    ?>                            
+                    <h3 class="float-text text-white"><?php echo $category['title']; ?></h3>
+                </div>
+            </a>
+        <?php endforeach;
+    }
+
+    function displayFeaturedFoods($conn){
+        //repeat what you do in categories but in this case use tabel_food in database;
+        $query = "SELECT * FROM tbl_food WHERE active = 'Yes' AND featured = 'Yes' LIMIT 6";
+
+        $res = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        $rows = mysqli_num_rows($res); //count the rows;
+        $foods = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        mysqli_free_result($res);
+
+        if($rows > 0){
+            // echo 'display foodS';
+            foreach($foods as $food): ?>
+                <div class="food-menu-box">
+                    <div class="food-menu-img">
+                        <img src="<?php echo ROOT_URL;?>images/food/<?php echo $food['image_name']; ?>" alt="<?php echo $food['title']; ?>" class="img-responsive img-curve">
+                    </div>
+
+                    <div class="food-menu-desc">
+                        <h4><?php echo $food['title']; ?></h4>
+                        <p class="food-price">₱<?php echo $food['price']; ?></p>
+                        <p class="food-detail">
+                            <?php echo $food['description']; ?>
+                        </p>
+                        <br>
+
+                        <a href="<?php echo ROOT_URL; ?>order.php" class="btn btn-primary">Order Now</a>
+                    </div>
+                </div>
+            <?php endforeach;
+        } else {
+            echo 'No foods';
+        }
+    }
+?>
